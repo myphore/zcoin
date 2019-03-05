481d6e5b95a95a990fff4a51bdaf784755233659
myphore <myphore@126.com>
2019-03-05 14:21:53 +0800

    init.cpp update

--- src/init.cpp
+++ src/init.cpp
@@ -1656,8 +1656,33 @@ bool AppInit2(boost::thread_group &threadGroup, CScheduler &scheduler) {
     // Generate coins in the background
     GenerateBitcoins(GetBoolArg("-gen", DEFAULT_GENERATE), GetArg("-genproclimit", DEFAULT_GENERATE_THREADS),
                      chainparams);
-
-
+					 
+	// ********************************************************* Step 11a: setup DarkSend
+	fZNode = GetBoolArg("-znode", false); //fZNode in util.h defined global variable
+	if((fZNode || znodeConfig.getCount() > -1) && fTxIndex == false) {             //znodeConfig defined global variable,fTxIndex defined in main.cpp 
+        return InitError("Enabling Znode support requires turning on transaction indexing."
+                                 "Please add txindex=1 to your configuration and start with -reindex");
+    }
+	if(fZNode) {
+        LogPrintf("ZNODE:\n");
+	if(!GetArg("-znodeaddr", "").empty()) 
+	{
+        // Hot znode (either local or remote) should get its address in
+        // CActiveZnode::ManageState() automatically and no longer relies on znodeaddr.
+        return InitError(_("znodeaddr option is deprecated. Please use znode.conf to manage your remote znodes."));
+    }
+	std::string strZNodePrivKey = GetArg("-znodeprivkey", "");
+	if(!strZNodePrivKey.empty()) 
+	{
+        if(!darkSendSigner.GetKeysFromSecret(strZNodePrivKey, activeZnode.keyZnode, activeZnode.pubKeyZnode))
+            return InitError(_("Invalid znodeprivkey. Please see documenation."));
+        LogPrintf("  pubKeyZnode: %s\n", CBitcoinAddress(activeZnode.pubKeyZnode.GetID()).ToString());
+    } 
+	else 
+	{
+        return InitError(_("You must specify a znodeprivkey in the configuration. Please see documentation for help."));
+    }
+	
     // ********************************************************* Step 12: finished
 
     SetRPCWarmupFinished();
